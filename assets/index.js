fc2f9d0b4db85dcd9eff66e0fc801d9320e42c44
Prokube <68293280+prokube@users.noreply.github.com>
2021-09-13 09:50:12 -0700

    more
    M O R E

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,10 @@
 function getMOTD() {
     var motds = Array("The best free utility client.", "Utility client for 1.8", "Mojang EULA-abiding Hypixel client.",
-        "Death to Pay2Pixel", "koljan gaming"); // type more in yourself
+        "Death to Pay2Pixel", "koljan gaming", "mechanical hands", "devs are based lmao", "Errorcrafter swag"); // type more in yourself
     return motds[Math.floor(Math.random() * motds.length)];
 }
 
+// "this message shouldn't appear :)"
 window.onload = () => {
     document.getElementById('motd').innerHTML = getMOTD();
 }
