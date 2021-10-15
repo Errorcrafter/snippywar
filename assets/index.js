5bbcc11123e0eb3f03447c3bc6518b54030f13b9
Steve <86112927+AcaiBerii@users.noreply.github.com>
2021-10-15 18:59:44 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,9 @@
 function getMOTD() {
-    var motds = Array("The best free utility client.", "Utility client for 1.8", "Mojang EULA-abiding Hypixel client.",
-        "Death to Pay2Pixel", "koljan gaming", "mechanical hands", "devs are based lmao", "Errorcrafter swag"); // type more in yourself
+    var motds = Array(
+        "The best free utility client.", "Utility client for 1.8", "Mojang EULA-abiding Hypixel client.",
+        "Death to Pay2Pixel", "koljan gaming", "mechanical hands", "devs are based lmao", "Errorcrafter swag",
+        "acaiberii sucks", "aaa a a aaaaaa"
+    ); // type more in yourself
     return motds[Math.floor(Math.random() * motds.length)];
 }
 
