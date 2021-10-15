3642c7e37c596548262b9575f29b366f79c7b363
Steve <86112927+AcaiBerii@users.noreply.github.com>
2021-10-15 19:24:52 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@ function getMOTD() {
     var motds = Array(
         "The best free utility client.", "Utility client for 1.8", "Mojang EULA-abiding Hypixel client.",
         "Death to Pay2Pixel", "koljan gaming", "mechanical hands", "devs are based lmao", "Errorcrafter swag",
-        "acaiberii sucks", "aa a a aaaaaaaa aa a a  a a aa"
+        "acaiberii sucks"
     ); // type more in yourself
     return motds[Math.floor(Math.random() * motds.length)];
 }
