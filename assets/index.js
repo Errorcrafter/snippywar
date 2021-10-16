0248d98a872467c5836cee8baaf6e2bc43141569
Steve <86112927+AcaiBerii@users.noreply.github.com>
2021-10-15 20:11:45 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@ function getMOTD() {
     var motds = Array(
         "The best free utility client.", "Utility client for 1.8", "Mojang EULA-abiding Hypixel client.",
         "Death to Pay2Pixel", "koljan gaming", "mechanical hands", "devs are based lmao", "Errorcrafter swag",
-        "acaiberii sucks"
+        "I can't find the module you're using on Lunar client! Send me your config!!!"
     ); // type more in yourself
     return motds[Math.floor(Math.random() * motds.length)];
 }
